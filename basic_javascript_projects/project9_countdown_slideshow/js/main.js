0a540699f6a0665092c81a016bf5e033fe9105a6
Turtle <mttaborturtle@gmail.com>
2020-12-04 18:19:59 -0800

    Add Project countdown slideshow
    Added all files for the countdown slideshow

---
+++ basic_javascript_projects/project9_countdown_slideshow/js/main.js
@@ -0,0 +1,34 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds == -1) {
+        alert("Time's up!");
+    }
+        }
+    tick();
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
